a67cdc68365aa9f652c9cf30c15317ba510c3662
fraponuvi <franz.nunez.v@uni.pe>
2026-09-07 15:46:00 -0500

    fix less for less or equal

--- 71-A.cpp
+++ 71-A.cpp
@@ -8,7 +8,7 @@ int main() {
     while (n--){
         string s;
         cin >> s;
-        if (s.size() < 10){
+        if (s.size() <= 10){
             cout << s << "\n";
         } else  {
             cout << s[0] << s.size() - 2 << s[s.size() - 1] << "\n";
