09af7753cd92af083ccef3c67dce53e81d7863ee
fraponuvi <franz.nunez.v@uni.pe>
2026-09-08 18:04:09 -0500

    Fixing flag for 96-A

--- 96-A.cpp
+++ 96-A.cpp
@@ -4,20 +4,17 @@ typedef long long ll;
 #define push_back as pb
 int main() {
     int contador_1 = 0, contador_0= 0;
-    bool flag_1 = false, flag_0 = false;
     string s;
     cin >> s;
     for (char c : s){
         if (c == '1') {
-            flag_1 = true;
             contador_1++;
             contador_0 = 0;
         } else {
-            flag_0 = true;
             contador_0++;
             contador_1 = 0;
         }
-        if ((contador_1 >= 7 && flag_0) || (contador_0 >= 7 && flag_1)){
+        if ((contador_1 >= 7) || (contador_0 >= 7)){
             cout << "YES\n";
             return 0;
         }
