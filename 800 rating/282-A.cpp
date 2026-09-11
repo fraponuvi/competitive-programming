87d2fe161c03392802c7b696d46e6ad1670f154d
fraponuvi <franz.nunez.v@uni.pe>
2026-09-10 21:32:47 -0500

    first try for 282-A

--- 800 rating/282-A.cpp
+++ 800 rating/282-A.cpp
@@ -3,6 +3,21 @@ using namespace std;
 typedef long long ll;
 #define push_back as pb
 int main() {
-    
+    int n, r = 0;
+    cin >> n;
+    while (n--){
+        string s;
+        cin >> s;
+        for (char c: s){
+            if (c == '+'){
+                r++;
+                break;
+            }else if (c == '-'){
+                r--;
+                break;
+            }
+        }
+    }
+    cout << r << "\n";  
     return 0;
 }
